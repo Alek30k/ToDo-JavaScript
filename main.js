81ca12c1bb41de54398e3c6e0aa8756babcef341
Alek30k <alemank23k@gmail.com>
2023-11-02 15:55:45 -0300

    last commit

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 const taskList = document.querySelector("#task-list");
+const newTaskList = document.querySelector("#new-task-input");
+const addTaskButton = document.querySelector("#add-task-button");
+
+const task = [];
